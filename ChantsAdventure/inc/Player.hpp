6f24705830ce78ba506f14ab88078224392da384
Marcos Carvajal <mcarvaja@coastal.edu>
2024-11-23 11:41:03 -0500

    Added an additional boolean to the Asset constructor, isDefensive. Using isOffensive and isDefensive will allow us to quickly identify weapons, armor/wearable equipment, and healing items. isOffensive and isDefensive default to false, so you don't have to set them both for every asset. You only need to enter one (true) for a weapon, and (false, true) for armor
    Added a vector of assets called _Equipment to
    store the player's equipped weapon and armor.
    
    The UseAsset function will check the boolean flags
    of the asset and either equip the weapon/armor to
    the corresponding slot in _Equipment, or use the
    item to heal. Weapons/Armor when switched to will
    move the current weapon/armor to the _assets
    vector.

--- ChantsAdventure/inc/Player.hpp
+++ ChantsAdventure/inc/Player.hpp
@@ -13,10 +13,19 @@ namespace chants
     {
     private:
         vector<Asset> _assets;
+        vector<Asset> _Equipment;
 
     public:
+        
+        int maxHealth;
+
         Player(string name, int health, int fightCoefficient);
         void AddAsset(Asset asset);
         int GetTotalAssetValue();
+
+        void UseAsset(Asset asset);
+        void RemoveAsset(int index);
+
+        void DisplayEquipment();
     };
 }
--- ChantsAdventure/src/Player.cpp
+++ ChantsAdventure/src/Player.cpp
@@ -1,10 +1,20 @@
 #include <Player.hpp>
+#include <iostream>
 
 namespace chants
 {
 
     Player::Player(string name, int health, int fightCoefficient) : Combatant(name, health, fightCoefficient)
     {
+        //An integer to track the maximum health value of the player
+        //Prevents health overflow via healing items
+        maxHealth = _health;
+        
+        //Starting assets for the player
+        Asset DefaultWeapon("Knife", "A small but sturdy blade.", 1, true);
+        Asset DefaultArmor("Pirate's Clothes", "The clothes of a former life.", 1, false, true);
+        _Equipment.push_back(DefaultWeapon);
+        _Equipment.push_back(DefaultArmor);
     }
 
     void Player::AddAsset(Asset asset)
@@ -20,5 +30,65 @@ namespace chants
             val += asset.GetValue();
         }
         return val;
+    }
+
+    void Player::RemoveAsset(int index)
+    {
+        _assets.erase(_assets.begin() + index);
+    }
+
+    void Player::DisplayEquipment()
+    {
+        cout << "Weapon: " << _Equipment[0].GetName() << endl;
+        cout << _Equipment[0].GetMessage() << endl;
+
+        cout << "Armor: " << _Equipment[1].GetName() << endl;
+        cout << _Equipment[1].GetMessage() << endl;
+    }
+
+    //Function that will use any asset. Usage is determined by the asset's boolean flags.
+    void Player::UseAsset(Asset asset)
+    {
+        //If the asset is a weapon
+        if (asset.isOffensive())
+        {
+            cout << "You equipped the \"" << asset.GetName() << "\"" << endl;
+            Asset tempAsset = _Equipment[0];
+            _Equipment[0] = asset;
+            AddAsset(tempAsset);
+
+        }
+        //If the asset is armor
+        else if (asset.isDefensive())
+        {
+            cout << "You equipped the \"" << asset.GetName() << "\"" << endl;
+            Asset tempAsset = _Equipment[1];
+            _Equipment[1] = asset;
+            AddAsset(tempAsset);
+        }
+        //If the asset is neither, it is a healing item.
+        else
+        {
+            //If the player's health is not full, a healing item can be used
+            if (_health < maxHealth)
+            {
+                _health += asset.GetValue();
+                
+                //If the healing item added health over the maximum, health is set to the maximum instead
+                if (_health > maxHealth)
+                {
+                    _health == maxHealth;
+                }
+
+                //The healing item is flagged as used
+                asset.hasBeenUsed = true;
+            }
+
+            //If the player's health is already full, a string is outputted and the item is not used
+            else
+            {
+                cout << "Your health is already full.";
+            }
+        }
     }
 }
