86c1f6af72cab2398d966ff20f8d8f20e5ce5f03
Lauren Harman <leharman@coastal.edu>
2024-12-04 18:36:01 -0500

    Added in an asset on accident here instad of a different branch

--- ChantsAdventure/app/game.cpp
+++ ChantsAdventure/app/game.cpp
@@ -265,7 +265,6 @@ int main()
 
     // FIXME: Balance weapons, armor, and monsters
     // FIXME: remove all the commented out code
-    // FIXME: do the assets need line breaks at the end?
 
     // Weapons
     Asset WoodenPlank("Wooden Plank", "It is warped and shattered at the ends.", 50, true);
@@ -318,7 +317,9 @@ int main()
     gameMap[11].AddAsset(&IronSword);
     gameMap[11].AddAsset(&IronChestPlate);
 
+    gameMap[12].AddAsset(&HealthPotion);
 
+    // Code for Node 13 is 
 
     gameMap[17].AddAsset(&OldSorcerersSword);
     gameMap[17].AddAsset(&HealthPotion);
