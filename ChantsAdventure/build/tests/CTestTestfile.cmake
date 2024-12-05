2af531045716f551afb6674119b17ef3ba1340f6
Librarylaptop <librarylaptop@scsccu.local>
2024-12-05 11:18:14 -0500

    remove build folder

--- ChantsAdventure/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gsmithson/dev/CSCI-207/final/CSCSI-207-final/ChantsAdventure/tests
-# Build directory: /home/gsmithson/dev/CSCI-207/final/CSCSI-207-final/ChantsAdventure/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
